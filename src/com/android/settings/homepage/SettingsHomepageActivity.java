7f6d83da1c679c0a244f9620d89aac2a6cc60b1a
00day0 <therandomuser11@gmail.com>
2020-01-01 21:27:32 +0100

    OneUI-like settings actionbar
    Signed-off-by: 00day0 <therandomuser11@gmail.com>
    Change-Id: Ie1a1fd42b57064a05d25821078f5725b4c776439
    Signed-off-by: LordShenron <shen.priyanshu@gmail.com>
    Signed-off-by: DrtSinX98 <pritish@stag-os.org>

--- src/com/android/settings/homepage/SettingsHomepageActivity.java
+++ src/com/android/settings/homepage/SettingsHomepageActivity.java
@@ -24,7 +24,7 @@ import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.Toolbar;
-
+import android.provider.Settings;
 import androidx.annotation.VisibleForTesting;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
@@ -65,6 +65,7 @@ public class SettingsHomepageActivity extends FragmentActivity {
         showFragment(new TopLevelSettings(), R.id.main_content);
         ((FrameLayout) findViewById(R.id.main_content))
                 .getLayoutTransition().enableTransitionType(LayoutTransition.CHANGING);
+
     }
 
     private void showFragment(Fragment fragment, int id) {
